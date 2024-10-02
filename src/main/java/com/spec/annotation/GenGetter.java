bbb1dd0a42f6eab331b351064269363b1cf43d96
Neeraj Surjaye <neerajsurjaye@gmail.com>
2024-10-02 20:29:39 +0530

    Created getter annotation

--- src/main/java/com/spec/annotation/GenGetter.java
+++ src/main/java/com/spec/annotation/GenGetter.java
@@ -1,5 +1,15 @@
 package com.spec.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.FIELD, ElementType.TYPE })
+@Retention(RetentionPolicy.SOURCE)
 public @interface GenGetter {
-    
+    /**
+     * Will generate a for all fields in class if placed on class level.
+     * To generate only for a field place the annotation on it.
+     */
 }
