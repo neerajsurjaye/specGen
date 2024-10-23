ddad8ba69ea9e27de5b463c8f885aba08ff39476
Neeraj Surjaye <neerajsurjaye@gmail.com>
2024-10-24 00:29:37 +0530

    Fixed Typos

--- src/main/java/com/spec/annotation/GenGetter.java
+++ src/main/java/com/spec/annotation/GenGetter.java
@@ -8,8 +8,8 @@ import java.lang.annotation.Target;
 @Target({ ElementType.FIELD, ElementType.TYPE })
 @Retention(RetentionPolicy.SOURCE)
 public @interface GenGetter {
-    /**
-     * Will generate a for all fields in class if placed on class level.
+    /*
+     * Will generate a getter for all fields in class if placed on class level.
      * To generate only for a field place the annotation on it.
      */
 }
