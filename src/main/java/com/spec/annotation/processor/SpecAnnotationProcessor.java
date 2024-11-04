d33054e2927e71dc098fab2693011edf213b6d74
Neeraj Surjaye <neerajsurjaye@gmail.com>
2024-11-05 00:32:54 +0530

    Implemented class to generate the getter code in a seprate file for now

--- src/main/java/com/spec/annotation/processor/SpecAnnotationProcessor.java
+++ src/main/java/com/spec/annotation/processor/SpecAnnotationProcessor.java
@@ -1,5 +1,7 @@
 package com.spec.annotation.processor;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -8,6 +10,15 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.tools.JavaFileObject;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
 import com.spec.annotation.GenGetter;
 
@@ -33,6 +44,7 @@ public class SpecAnnotationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
+        System.out.println("Processor called");
         // generate getter for elements annotated with @Getter annotation
         for (Element element : roundEnv.getElementsAnnotatedWith(GenGetter.class)) {
             generateGetter(element);
@@ -72,16 +84,105 @@ public class SpecAnnotationProcessor extends AbstractProcessor {
             return;
         }
 
-        String className = ((TypeElement) element.getEnclosingElement()).getQualifiedName().toString();
+        TypeElement enclosingElement = (TypeElement) element.getEnclosingElement();
+        // todo remove this gen from end
+        String className = enclosingElement.getQualifiedName().toString() + "gen";
         String fieldName = element.getSimpleName().toString();
-        String methodName = capitalizeFieldName(fieldName);
+        String capitalizedFieldName = capitalizeFieldName(fieldName);
+        String getterName = "get" + capitalizedFieldName;
 
-        String methodCode = String.format("public %s %s (){return this.%s}", element.asType(), methodName, fieldName);
+        // flag passed as 0 means I dont want any special features.
+        ClassWriter classWriter = new ClassWriter(0);
+
+        // internally classes are represented using / instead of ".".
+        String classInternalName = className.replace(".", "/");
+
+        // todo find if the opcode version can be changed
+        classWriter.visit(
+                Opcodes.V1_8,
+                Opcodes.ACC_PUBLIC,
+                classInternalName,
+                null,
+                "java/lang/Object",
+                null);
+
+        MethodVisitor methodVisitor = classWriter.visitMethod(
+                Opcodes.ACC_PUBLIC,
+                getterName,
+                "()" + getDescriptor(element.asType()),
+                null,
+                null);
+
+        methodVisitor.visitCode();
+
+        methodVisitor.visitVarInsn(Opcodes.ALOAD, 0);
+
+        methodVisitor.visitFieldInsn(
+                Opcodes.GETFIELD,
+                classInternalName,
+                fieldName,
+                getDescriptor(element.asType()));
+
+        methodVisitor.visitInsn(Opcodes.RETURN);
+
+        methodVisitor.visitMaxs(1, 1);
+
+        methodVisitor.visitEnd();
+
+        classWriter.visitEnd();
+
+        writeClass(classInternalName, classWriter.toByteArray());
 
     }
 
     private String capitalizeFieldName(String fieldName) {
         return fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+    }
+
+    // todo visit this again and get info about type discriptor
+    private String getDescriptor(TypeMirror type) {
+
+        if (type.getKind().isPrimitive()) {
+            switch (type.getKind()) {
+                case INT:
+                    return "I";
+                case BOOLEAN:
+                    return "Z";
+                case CHAR:
+                    return "C";
+                case BYTE:
+                    return "B";
+                case SHORT:
+                    return "S";
+                case LONG:
+                    return "J";
+                case FLOAT:
+                    return "F";
+                case DOUBLE:
+                    return "D";
+                default:
+                    throw new IllegalArgumentException("Unknown primitve type : " + type);
+
+            }
+        } else if (type.getKind() == TypeKind.ARRAY) {
+            ArrayType arrayType = (ArrayType) type;
+            return "[" + getDescriptor(arrayType.getComponentType());
+        } else {
+            String internalName = ((DeclaredType) type).asElement().toString().replace(".", "/");
+            return "L" + internalName + ";";
+        }
+
+    }
+
+    private void writeClass(String className, byte[] bytecode) {
+        try {
+            JavaFileObject fileObject = processingEnv.getFiler().createClassFile(className);
+            try (Writer writer = fileObject.openWriter()) {
+                writer.write(new String(bytecode));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
